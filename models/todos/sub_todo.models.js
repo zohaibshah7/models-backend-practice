20019e32198996f7c63bbeacd34c3a2df340218f
Syed Zohaib Shah <syedzohaibshah969@gmail.com>
2025-02-05 17:06:08 +0500

    Todo model

---
+++ models/todos/sub_todo.models.js
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose';
+
+const subTodoSchema = new mongoose.Schema(
+  {
+    content: {
+      type: String,
+      required: true,
+    },
+    complete: {
+      type: Boolean,
+      default: false,
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+    },
+    todo: {
+      type: mongoose.Schema.Types,
+      ObjectId,
+      ref: 'Todo',
+    },
+  },
+  { timestamps: true }
+);
+
+export const SubTodo = mongoose.Schema('SubTodo', subTodoSchema);
